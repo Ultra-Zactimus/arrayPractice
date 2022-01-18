f4d6584583a0b0b1f1fed54ea9256c8b7b37ba30
Ultra Zacky <ultrazacky@gmail.com>
2022-01-18 13:46:32 -0800

    add array info to ul lists on html page.
    Co-Authored-by: Jake Haley <jakechaley@gmail.com>
    Co-Authored-by: Alex McKnight <alex.mcknight11@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -9,23 +9,13 @@ $(document).ready(function(){
     const result3 = $("input.thirdResult").val();
 
     totalResults.push(result1, result2, result3);
-
-    const result1Show = [];
-    const result2Show = [];
-    const results3Show = [];
-
-    result1Show.push(totalResults[0]);
-    result2Show.push(totalResults[1]);
-    result3Show.push(totalResults[2]);
+    
+    $(".list1").text(totalResults[0]);
+    $(".list2").text(totalResults[1]);
+    $(".list3").text(totalResults[2]);
 
     event.preventDefault();
-    
-    console.log(result1Show);
 
-    
-
-
-
-});
+  });
 
 });
